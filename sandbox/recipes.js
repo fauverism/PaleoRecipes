606aa2557641e452aea7478e251629b9eab6a247
Robert Fauver <robertfauver@gmail.com>
2013-09-06 15:52:48 -0400

    Update recipes.js

--- sandbox/recipes.js
+++ sandbox/recipes.js
@@ -1,2 +1,10 @@
 {
+    "id": "1",
+    "meal": "Snack",
+    "title": "Cookies",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vehicula sit amet nisi nec tincidunt. Pellentesque hendrerit eros odio, iaculis condimentum ligula gravida nec. Vestibulum pharetra lobortis arcu, non semper quam malesuada ut. Sed aliquam eros ac sem rutrum, quis pretium sem volutpat. Vestibulum sit amet lectus ut mauris viverra cursus vel ut libero. Fusce at lacinia quam. Maecenas eu ullamcorper lacus. Donec volutpat eu enim sit amet rhoncus. Sed laoreet orci quis nisi elementum, eget aliquet arcu porta. Cras nunc turpis, volutpat id quam a, aliquet pretium tellus. Proin viverra dignissim turpis eu viverra. Nulla in nibh et justo dignissim tincidunt bibendum non velit. Integer porttitor sem dolor, vitae facilisis odio gravida sit amet."
+    "ingredients": [
+      "amount": "1",
+      
+    ]
 }
